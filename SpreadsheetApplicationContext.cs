337ec603c0f2f2e935f19c86de885a6431db7afe
Ryan Williams <rdubz.801smashin@gmail.com>
2016-04-03 13:51:32 -0600

    Used to open multiple GUI's at once.

---
+++ SpreadsheetApplicationContext.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpreadsheetGUI
+{
+    class SpreadsheetApplicationContext : ApplicationContext
+    {
+        // Number of open forms
+        private int windowCount = 0;
+
+        // Singleton ApplicationContext
+        private static SpreadsheetApplicationContext context;
+
+        /// <summary>
+        /// Private constructor for singleton pattern
+        /// </summary>
+        private SpreadsheetApplicationContext()
+        {
+        }
+
+        /// <summary>
+        /// Returns the one DemoApplicationContext.
+        /// </summary>
+        public static SpreadsheetApplicationContext GetContext()
+        {
+            if (context == null)
+            {
+                context = new SpreadsheetApplicationContext();
+            }
+            return context;
+        }
+
+        /// <summary>
+        /// Runs a form in this application context
+        /// </summary>
+        public void RunNew()
+        {
+            // Create the window and the controller
+            Form1 window = new Form1();
+            new Controller(window);
+
+            // One more form is running
+            windowCount++;
+
+            // When this form closes, we want to find out
+            window.FormClosed += (o, e) => { if (--windowCount <= 0) ExitThread(); };
+
+            // Run the form
+            window.Show();
+        }
+
+        public void RunNew(string filename)
+        {
+            // Create the window and the controller
+            Form1 window = new Form1();
+            new Controller(window, new StreamReader(filename));
+
+            // One more form is running
+            windowCount++;
+
+            // When this form closes, we want to find out
+            window.FormClosed += (o, e) => { if (--windowCount <= 0) ExitThread(); };
+
+            // Run the form
+            window.Show();
+        }
+
+    }
+}
