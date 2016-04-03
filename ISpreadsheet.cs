4a248142a8b459ecacdc013924bd12d4ae4618d8
Ryan Williams <rdubz.801smashin@gmail.com>
2016-04-03 13:53:42 -0600

    An interface that the view implements. Controller has a reference to this, in order for communication between view and controller.

---
+++ ISpreadsheet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetGUI
+{
+    public interface ISpreadsheet
+    {
+        event Func<string , object> UpdateContextBox;
+
+        event Func<string, object> UpdateValueBox;
+
+        event Action<string, string> SetContentsInSpreadSheet;
+
+        event Action<string> FileChosenEvent;
+
+        event Action CloseEvent;
+
+        event Action<string> SaveEvent;
+
+        event Action NewEvent;
+
+        event Action HelpMessage;
+
+        void OpenNew(string filename);
+
+        event Func <bool>CheckSaved;
+
+        string Message { set; }
+
+        void DoClose();
+
+        void UpdatePanel(object col, object row, string value);
+
+        void OpenNew();
+
+    }
+}
